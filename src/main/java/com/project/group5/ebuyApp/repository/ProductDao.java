8ae0647a99a49359fa1893fc8438cca744bdc794
fissehaye <fissehaye@gmail.com>
2018-09-24 10:53:29 -0500

    Updated configuration file

--- src/main/java/com/project/group5/ebuyApp/repository/ProductDao.java
+++ src/main/java/com/project/group5/ebuyApp/repository/ProductDao.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductDao extends JpaRepository<Product, Long> {
+public interface ProductDao extends JpaRepository<Product, Integer> {
 }
